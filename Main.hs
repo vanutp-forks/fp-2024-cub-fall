8c6bb8077ece3516e61e20f86dded1c73cda6c5b
Ekaterina Verbitskaia <kajigor@gmail.com>
2024-09-13 18:04:39 +0200

    HW01

---
+++ Main.hs
@@ -0,0 +1,168 @@
+module Main where
+
+import Control.Monad (unless)
+import Text.Printf (printf)
+
+short :: [a] -> Bool
+short = undefined 
+
+lovely :: [Int] -> Bool
+lovely = undefined 
+
+rightTriangles :: [(Int, Int, Int)]
+rightTriangles = undefined 
+
+fizzBuzz :: [String]
+fizzBuzz = undefined 
+
+ageOn :: String -> Float -> Float
+ageOn planet ageInSeconds = undefined 
+
+isLeapYear :: Int -> Bool
+isLeapYear year = undefined 
+
+main = do
+  runTests
+  putStrLn "Done"
+
+runTests = do
+  runShortTests
+  runLovelyTests
+  runRightTriangleTests
+  runFizzBuzzTests
+  runAgeOnTests
+  runIsLeapYearTests
+  where
+    describeFailure functionName errorMsg input exp actual =
+      printf
+        "Test for a function %s has failed:\n  %s\n  Input: %s\n  Expected: %s\n  But got: %s\n"
+        functionName
+        errorMsg
+        (show input)
+        (show exp)
+        (show actual)
+
+    eqTest funName errorMsg input exp actual =
+      unless (actual == exp) $ describeFailure funName errorMsg input exp actual
+
+    runShortTests =
+      mapM_ test cases
+      where
+        test (input, exp) = eqTest "short" "unexpected result" input exp (short input)
+        cases = [([], True), ([1], True), ([1, 2], True), ([1, 2, 3], False), ([1, 2, 3, 4], False), ([1 ..], False)]
+
+    runLovelyTests =
+      mapM_ test cases
+      where
+        test (input, exp) = eqTest "lovely" "unexpected result" input exp (lovely input)
+        cases = [([], True), ([1], True), ([1, 2], True), ([1, 2, 3], False), ([1, 2, 3, 4], False), ([1, 2, 14, 4], True), ([1 ..], False)]
+
+    runRightTriangleTests = do
+      let n = 20
+      let exp = [(3, 4, 5), (6, 8, 10), (5, 12, 13), (9, 12, 15), (8, 15, 17), (12, 16, 20), (15, 20, 25), (7, 24, 25), (10, 24, 26), (20, 21, 29), (18, 24, 30), (16, 30, 34), (21, 28, 35), (12, 35, 37), (15, 36, 39), (24, 32, 40), (9, 40, 41), (27, 36, 45), (30, 40, 50), (14, 48, 50)]
+      unless (take n rightTriangles == exp) $
+        putStrLn $
+          printf
+            "rightTriangles produces a wrong result. The first %s answers are supposed to be: %s"
+            (show n)
+            (show exp)
+
+    runFizzBuzzTests = do
+      let n = 20
+      let exp = ["1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz", "16", "17", "Fizz", "19", "Buzz"]
+      unless (take n fizzBuzz == exp) $
+        putStrLn $
+          printf
+            "fizzBuzz produces a wrong result. The first %s answers are supposed to be: %s"
+            (show n)
+            (show exp)
+
+    runAgeOnTests =
+      mapM_ test cases
+      where
+        test (planet, seconds, exp) =
+          let actual = ageOn planet seconds
+           in unless (actual `isEqual` exp) $ describeFailure "ageOn" (printf "Wrong age on planet %s" planet :: String) seconds exp actual
+          where
+            isEqual x y = roundTo 2 x == roundTo 2 y
+            roundTo n = (/ 10 ^ n) . fromIntegral . round . (* 10 ^ n)
+        cases =
+          [ ( "Earth",
+              1000000000,
+              31.69
+            ),
+            ( "Mercury",
+              2134835688,
+              280.88
+            ),
+            ( "Venus",
+              189839836,
+              9.78
+            ),
+            ( "Mars",
+              2129871239,
+              35.88
+            ),
+            ( "Jupiter",
+              901876382,
+              2.41
+            ),
+            ( "Saturn",
+              2000000000,
+              2.15
+            ),
+            ( "Uranus",
+              1210123456,
+              0.46
+            ),
+            ( "Neptune",
+              1821023456,
+              0.35
+            )
+          ]
+
+    runIsLeapYearTests =
+      mapM_ test cases
+      where
+        test (errorMsg, input, exp) =
+          let actual = isLeapYear input
+           in unless (actual == exp) $ describeFailure "isLeapYear" errorMsg input exp actual
+
+        cases =
+          [ ( "year not divisible by 4 in common year",
+              2015,
+              False
+            ),
+            ( "year divisible by 2, not divisible by 4 in common year",
+              1970,
+              False
+            ),
+            ( "year divisible by 4, not divisible by 100 in leap year",
+              1996,
+              True
+            ),
+            ( "year divisible by 4 and 5 is still a leap year",
+              1960,
+              True
+            ),
+            ( "year divisible by 100, not divisible by 400 in common year",
+              2100,
+              False
+            ),
+            ( "year divisible by 100 but not by 3 is still not a leap year",
+              1900,
+              False
+            ),
+            ( "year divisible by 400 in leap year",
+              2000,
+              True
+            ),
+            ( "year divisible by 400 but not by 125 is still a leap year",
+              2400,
+              True
+            ),
+            ( "year divisible by 200, not divisible by 400 in common year",
+              1800,
+              False
+            )
+          ]
--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# fp-2024-cub-fall
-This is a supplementary repository for the Introduction to Functional Programming course read at Constructor University Bremen at the fall term of 2024. 
+# HW01
+## Soft deadline: 23:59 17.09.2024
+
+1. (1 point) Implement the predicate `short` which checks that the input list has length less than 3. 
+
+2. (1 point) Implement the predicate `lovely` that checks that a list is either short (length <3) or has the number `14` as its third element. 
+
+3. (1 point) Create an infinite list of possible integer lengths of sides of a right triangle.
+    * make sure there are no repetitions.
+    * make sure that the lengths are ordered.
+
+4. (1 point) Create an infinite list of string values for a Fizz-Buzz challenge
+    * `["1", "2", "Fizz", "4", "Buzz", ..., "14", "FizzBuzz", ...]`
+    *  use `show` to turn a number into a string
+
+5. (1 point) Given an age in seconds, calculate how old (in years) someone would be on a different planet. Implement a function `ageOn planet ageInSeconds` which returns the age in years represented as a floating point number of type `Float`. Planet name is a `String`, case sensitive. An error should be reported if an unrecognised planet is passed into. If someone tried to find their age on Pluto, explain to them that it's not a planet. 
+
+   * Mercury: orbital period 0.2408467 Earth years
+   * Venus: orbital period 0.61519726 Earth years
+   * Earth: orbital period 1.0 Earth years, 365.25 Earth days, or 31557600 seconds
+   * Mars: orbital period 1.8808158 Earth years
+   * Jupiter: orbital period 11.862615 Earth years
+   * Saturn: orbital period 29.447498 Earth years
+   * Uranus: orbital period 84.016846 Earth years
+   * Neptune: orbital period 164.79132 Earth years
+
+6. (1 point) Given a year, report if it is a leap year (function `isLeapYear year`). A leap year in the Gregorian calendar occurs:
+
+   * on every year that is evenly divisible by 4
+   * except every year that is evenly divisible by 100
+   * unless the year is also evenly divisible by 400
+
+   For example, 1997 is not a leap year, but 1996 is. 1900 is not a leap year, but 2000 is. Report an error, if the year is negative. 
+
+## Notes 
+
+* Make a fork of this repository and checkout the branch `HW01`.
+* Write your code in the `Main.hs` file: replace `undefined` with your definitions.
+* Make sure that your tests pass, i.e. running `main` (or `./Main`) only outputs `Done`.
+* If you need more tests, add them, but don't delete the ones which are already in the file. 
+* When finished, open a pull request into the main repo. Make sure to put your name in the title of the PR.  

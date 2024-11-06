642b653fab480692b223d9f6e33a308c01a87528
Ekaterina Verbitskaia <kajigor@gmail.com>
2024-11-06 08:13:15 +0100

    Fix build

--- PBT/test/Test.hs
+++ PBT/test/Test.hs
@@ -1,13 +1,11 @@
 import Test.Tasty
 
 import qualified Test.Sort
-import qualified Test.List
 import qualified Test.Unit
 
 main :: IO ()
 main = do
   defaultMain (testGroup "All Tests"
                 [ testGroup "Sort" Test.Sort.props
-                , testGroup "List" Test.List.props
                 , testGroup "Unit" Test.Unit.unitTests
                 ])

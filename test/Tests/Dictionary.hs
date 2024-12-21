eee7eb09a3c93448617f530a9e3fe81f32e598c4
Ivan Filipenkov <hello@vanutp.dev>
2024-12-21 18:55:39 +0100

    add PBT

--- test/Tests/Dictionary.hs
+++ test/Tests/Dictionary.hs
@@ -1,13 +1,17 @@
 module Tests.Dictionary (testDictionary) where
 
+import Hedgehog
+import qualified Hedgehog.Gen as Gen
+import qualified Hedgehog.Range as Range
 import Test.Tasty (TestTree, testGroup)
 import Test.Tasty.HUnit (testCase, assertEqual)
+import Test.Tasty.Hedgehog
 import qualified Data.HashSet as HashSet
 import qualified Dictionary
 
 testDictionary :: TestTree
 testDictionary =
-  testGroup "Dictionary" [testLoad, testAdd, testRemove, testHasWord]
+  testGroup "Dictionary" [testLoad, testAdd, testRemove, testHasWord, testRandom]
   where
     testLoad = testCase "load" $ do
         let Just dict = Dictionary.fromJsonString "{\"hello\": 1, \"world\": 2}"
@@ -37,3 +41,19 @@ testDictionary =
         assertEqual "There is a \"hello\" in the new dictionary" (Dictionary.hasWord dict' "hello") True
         assertEqual "There is no \"world\" in the new dictionary" (Dictionary.hasWord dict' "world") False
         assertEqual "There is no \"test\" in the new dictionary" (Dictionary.hasWord dict' "test") False
+    testRandom = testProperty "random" $ property $ do
+        words <- forAll $ Gen.list (Range.linear 100 1000) (Gen.string (Range.linear 1 4) Gen.digit)
+        let dict = Dictionary.fromList words
+        assert $ all (Dictionary.hasWord dict) words
+
+        let firstWord = head words
+        let firstWordCount = length $ filter (firstWord ==) words
+        assert $ Dictionary.getWordCount dict firstWord == firstWordCount
+
+        let dict' = Dictionary.removeWord dict firstWord
+        assert $ Dictionary.getWordCount dict' firstWord == 0
+        assert $ not $ Dictionary.hasWord dict' firstWord
+
+        let dict'' = Dictionary.addWord dict' firstWord
+        assert $ Dictionary.getWordCount dict'' firstWord == 1
+        assert $ Dictionary.hasWord dict'' firstWord
--- test/Tests/Utils.hs
+++ test/Tests/Utils.hs
@@ -1,12 +1,17 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 module Tests.Utils (testUtils) where
 
+import Hedgehog
+import qualified Hedgehog.Gen as Gen
+import qualified Hedgehog.Range as Range
 import Test.Tasty (TestTree, testGroup)
 import Test.Tasty.HUnit (testCase, (@?=))
+import Test.Tasty.Hedgehog
 import Utils
 
 testUtils :: TestTree
 testUtils =
-  testGroup "Utils" [testWrapSimple, testWrapLong]
+  testGroup "Utils" [testWrapSimple, testWrapLong, testWrapRandom]
   where
     testWrapSimple = testCase "wrap: simple" $ do
       let input = "Hello, world!"
@@ -16,4 +21,10 @@ testUtils =
       let input = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam venenatis erat dolor, quis hendrerit dui ultrices non. Morbi fringilla a quam non auctor. Proin tincidunt ante non dictum dapibus. Quisque at sodales urna. Aenean sit amet libero quis nunc venenatis aliquet. Donec convallis, leo eu malesuada placerat, tortor dui faucibus felis, nec aliquam erat sapien id mauris. Pellentesque sed vulputate eros, eget porta metus. Nullam vitae porta dui. Morbi tempus velit a arcu rutrum, in laoreet ex ultrices. Ut nec sapien vel mi vulputate consectetur at vitae ipsum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer sit amet eros quis justo pulvinar maximus. Ut ac nunc quis ligula placerat vehicula."
       let expected = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam venenatis erat\ndolor, quis hendrerit dui ultrices non. Morbi fringilla a quam non auctor. Proin\ntincidunt ante non dictum dapibus. Quisque at sodales urna. Aenean sit amet\nlibero quis nunc venenatis aliquet. Donec convallis, leo eu malesuada placerat,\ntortor dui faucibus felis, nec aliquam erat sapien id mauris. Pellentesque sed\nvulputate eros, eget porta metus. Nullam vitae porta dui. Morbi tempus velit a\narcu rutrum, in laoreet ex ultrices. Ut nec sapien vel mi vulputate consectetur\nat vitae ipsum. Pellentesque habitant morbi tristique senectus et netus et\nmalesuada fames ac turpis egestas. Integer sit amet eros quis justo pulvinar\nmaximus. Ut ac nunc quis ligula placerat vehicula.\n"
       wrap 80 input @?= expected
+    testWrapRandom = testProperty "wrap: random" $ property $ do
+      text <- forAll $ unwords <$> Gen.list (Range.linear 1 500) (Gen.string (Range.linear 1 100) Gen.alphaNum)
+      wrapWidth <- forAll $ Gen.int (Range.linear 10 200)
+      let wrapped = wrap wrapWidth text
+      assert $ all (\line -> length line <= wrapWidth || length (words line) == 1) (lines wrapped)
+      assert $ unwords (words wrapped) == text
 
